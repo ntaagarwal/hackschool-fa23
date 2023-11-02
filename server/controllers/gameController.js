79099fe71be00720c049f1f3951e2c418a85b13f
ntaagarwal <n3agarwa@ucsd.edu>
2023-11-01 19:11:40 -0700

    week 4 changes

--- server/controllers/gameController.js
+++ server/controllers/gameController.js
@@ -1,5 +1,30 @@
+const getGame = async (req, res) => {
+    const game = {
+        sentence: "Hello there!",
+        correctCharacters: 14,
+        incorrectCharacters: 15,
+        wpm: 100,
+        time: 13.33
+    };
+    res.status(200).json(game);
+}
 
+const postGame = async (req, res) => {
+    const gamedata = req.body;
+    const invalid = "invalid input";
+    const {sentence, correctCharacters, incorrectCharacters, wpm, time} = gamedata; 
+    if(!sentence|| correctCharacters == null || incorrectCharacters == null || !wpm || !time){
+        res.status(400).json({error: invalid});
+        return;
+    }
+    const sl = gamedata.sentence.length;
+    if(sl != (correctCharacters + incorrectCharacters)){
+        res.status(400).json({error: "hi"});
+        return; 
+    }
+    // database call
 
+    res.status(200).json({message: "success"});
+}
 
-
-module.exports = {  };
+module.exports = {getGame, postGame};
